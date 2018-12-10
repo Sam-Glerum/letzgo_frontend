7ab3c8d18df43448eb5dd52cb3f2b9a43bc35e15
Sam Glerum <samuelglerum@hotmail.com>
2018-12-10 15:45:25 +0100

    created componets for login,register,concertDetail, artist and ticket. fixed functionality for showing all concerts

--- letzgo/src/app/app-routing.module.ts
+++ letzgo/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Component import
+import { ConcertComponent } from "./concert/concert.component";
+import { ConcertDetailComponent } from "./concert-detail/concert-detail.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ArtistComponent } from "./artist/artist.component";
+import { TicketComponent } from "./ticket/ticket.component";
+
+
+const routes: Routes = [
+  {path: 'concerts', component: ConcertComponent},
+  {path: 'concertDetail/:id', component: ConcertDetailComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'artists', component: ArtistComponent},
+  {path: 'tickets', component: TicketComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- letzgo/src/app/app.module.ts
+++ letzgo/src/app/app.module.ts
@@ -8,18 +8,28 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
 import { ConcertComponent } from './concert/concert.component';
 import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ArtistComponent } from './artist/artist.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { ConcertDetailComponent } from './concert-detail/concert-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConcertComponent
+    ConcertComponent,
+    LoginComponent,
+    RegisterComponent,
+    ArtistComponent,
+    TicketComponent,
+    ConcertDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
