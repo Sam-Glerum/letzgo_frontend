2dc7cda3381f166b0a361f18619ead333afbc719
Sam Glerum <samuelglerum@hotmail.com>
2018-12-11 01:07:23 +0100

    fixed artist page:

--- letzgo/src/components/artist-detail/artist-detail.component.ts
+++ letzgo/src/components/artist-detail/artist-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Artist} from "../../artist";
+import {ArtistService} from "../../app/services/artistService/artist.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-artist-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-detail.component.css']
 })
 export class ArtistDetailComponent implements OnInit {
+  artist = Artist;
+  artistId = '';
 
-  constructor() { }
+  constructor(private artistService: ArtistService, private route:ActivatedRoute) { }
+
+  getArtistId() {
+    this.route.params.subscribe(param => {
+      this.artistId = param['id'];
+    });
+  }
+
+  getArtist(): void {
+    this.artistService.getArtist(this.artistId)
+      .subscribe(artist => {
+        this.artist = artist;
+        console.log(artist);
+      });
+  }
 
   ngOnInit() {
+    this.getArtistId();
+    this.getArtist();
   }
 
 }
--- letzgo/src/components/concert-detail/concert-detail.component.ts
+++ letzgo/src/components/concert-detail/concert-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConcertComponent } from "../concert/concert.component";
+import {ConcertService} from "../../app/services/concertService/concert.service";
+import {Concert} from "../../app/concert";
+import {ActivatedRoute} from "@angular/router";
+import {TicketService} from "../../app/services/ticketService/ticket.service";
 
 @Component({
   selector: 'app-concert-detail',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./concert-detail.component.css']
 })
 export class ConcertDetailComponent implements OnInit {
+  constructor(private concertService: ConcertService, private route:ActivatedRoute, private ticketService: TicketService) { }
 
-  constructor() { }
+  concert = Concert;
+  concertId = '';
 
   ngOnInit() {
+    this.getConcertId();
+    this.getConcert();
+  }
+
+  getConcertId(): void{
+    this.route.params.subscribe(param => {
+    this.concertId += param['id'];
+    });
+  }
+
+  getConcert(): void {
+
+    this.concert = this.concertService.getConcert(this.concertId)
+      .subscribe((concert => {
+        this.concert = concert;
+      }));
+  };
+
+  buyTicket(): void {
+    console.log("concert: " + this.concertId);
+    console.log("username: " + localStorage.getItem("Username"));
+    this.ticketService.addTicket(this.concertId, localStorage.getItem("Username"));
   }
 
 }
